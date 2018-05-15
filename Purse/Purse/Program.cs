3025c92205c486e71b668ccb75175b9630b8e5de
HvozdykNazar <Aligaotr@ukr.net>
2018-05-16 00:14:05 +0300

    conflict fixed by nazar

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CardGold;
 using MoneyClass;
 namespace Purse
 {
@@ -10,6 +11,23 @@ namespace Purse
     {
         static void Main(string[] args)
         {
+            //Hello
+            //this is project by Badz and Hvozdyk
+            //task for Hvozdyk
+
+            //Hvozdyk add reference on Gold Card
+            Gold card = new Gold();
+
+            card.owner = "Hvozdyk";
+            card.age = 25;
+
+            //get info
+            Console.WriteLine("Owner of card information:");
+            Console.WriteLine(card.owner);
+            Console.WriteLine(card.age);
+
+
+            // Badz Add class
 //Badz ADDDDD
             Money m = new Money();
             m.StartMoney = 100;
@@ -20,6 +38,7 @@ namespace Purse
             Console.WriteLine(m.startMoney);
             Console.WriteLine("End money:");
             Console.WriteLine(m.EndMoney);
+
         }
     }
 }
