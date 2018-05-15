b98dc08437f82b1a7a0b80f002decd04e1575d56
DESKTOP-Q5KV6QD\Andrii <andriy2021@gmail.com>
2018-05-16 00:03:50 +0300

    badz commit

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CardGold;
 namespace Purse
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Hello
-            //this is project by Badz and Hvozdyk
-            //task for Hvozdyk
+//Hvozdyk add 
+            Gold card = new Gold();
+
+            card.owner = "Hvozdyk";
+            card.age = 25;
+
+            //get info
+            Console.WriteLine("Owner of card information:");
+            Console.WriteLine(card.owner);
+            Console.WriteLine(card.age);
         }
     }
 }
