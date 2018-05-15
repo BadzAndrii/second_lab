77581263883aab46bc03659401c8f5ad64a232f0
DESKTOP-Q5KV6QD\Andrii <andriy2021@gmail.com>
2018-05-15 21:25:45 +0300

    create project

---
+++ Purse/Purse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello
+            //this is project by Badz and Hvozdyk
+        }
+    }
+}
