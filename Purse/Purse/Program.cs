d870baab43fb66f8d496e8b08ebe6ff4b85f53dd
DESKTOP-Q5KV6QD\Andrii <andriy2021@gmail.com>
2018-05-16 15:04:40 +0300

    teeessteet

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CardGold;
+using MoneyClass;
 namespace Purse
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Hello
+            //this is project by Badz and Hvozdyk
+            //Hvozdyk add reference on Gold Card
             //Hvozdyk add 
             Gold card = new Gold();
 
@@ -20,6 +24,25 @@ namespace Purse
             Console.WriteLine("Owner of card information:");
             Console.WriteLine(card.owner);
             Console.WriteLine(card.age);
+
+
+            // Badz Add class
+//Badz ADDDDD
+             //test2
+            Money m = new Money();
+            m.StartMoney = 100;
+            m.AddMoney = 500;
+            
+            //get info
+            Console.WriteLine("Start money:");
+            Console.WriteLine(m.startMoney);
+            Console.WriteLine("End money:");
+            Console.WriteLine(m.EndMoney);
+            //get info
+            Console.WriteLine("Owner of card information:");
+            Console.WriteLine(card.owner);
+            Console.WriteLine(card.age);
+            //teeeseeet
         }
     }
 }
