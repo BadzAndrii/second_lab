52aa323bce3d3332e5dc592783e6ce73c8e25eca
DESKTOP-Q5KV6QD\Andrii <andriy2021@gmail.com>
2018-05-16 10:32:08 +0300

    test by badz bygagagagaga

---
+++ MoneyClass/MoneyClass/Money.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyClass
+{
+    public class Money
+    {
+        //create Money class
+        public int startMoney;
+        public int endMoney;
+        public int addMoney;
+
+        public int StartMoney
+        {
+            get { return startMoney; }
+            set { startMoney = value; }
+        }
+        public int AddMoney
+        {
+            set { addMoney = value; }
+        }
+        public int EndMoney
+        {
+            get { return endMoney = startMoney + addMoney; }
+        }
+
+    }
+}
--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CardGold;
+using MoneyClass;
 namespace Purse
 {
     class Program
@@ -12,14 +13,24 @@ namespace Purse
         {
             //Hvozdyk add 
             Gold card = new Gold();
+            //Hello
+            //this is project by Badz and Hvozdyk
+            //task for Hvozdyk
 
             card.owner = "Hvozdyk";
             card.age = 25;
-
+            Money m = new Money();
+            m.StartMoney = 100;
+            m.AddMoney = 500;
+            
             //get info
             Console.WriteLine("Owner of card information:");
             Console.WriteLine(card.owner);
             Console.WriteLine(card.age);
+            Console.WriteLine("Start money:");
+            Console.WriteLine(m.startMoney);
+            Console.WriteLine("End money:");
+            Console.WriteLine(m.EndMoney);
         }
     }
 }
